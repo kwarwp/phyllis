f510caf9b83dca9280d62136fc37bea67d8f4bed
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-10-26 15:24:54 -0300

    Saved roxanne/__log__.py @2020-10-26 18:24

--- roxanne/__log__.py
+++ roxanne/__log__.py
@@ -41,3 +41,19 @@ IndentationError: expected an indented block
          ^
 SyntaxError: invalid syntax
 '''},
+{'date': 'Mon Oct 26 2020 15:24:53.742 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 180
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 310
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 282
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 299
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 22
+    mar.vai ()
+NameError: name 'mar' is not defined
+'''},
