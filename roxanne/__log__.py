9f6217e25d2dd210a48db4a31d24d019f159cbcd
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-10-26 14:45:16 -0300

    Created roxanne/__log__.py @2020-10-26 17:45

---
+++ roxanne/__log__.py
@@ -0,0 +1,8 @@
+
+{'date': 'Mon Oct 26 2020 14:45:14.463 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 4
+  MAR = https://i.imgur.com/fk9FgaR.jpg
+              ^
+SyntaxError: invalid syntax
+'''},
